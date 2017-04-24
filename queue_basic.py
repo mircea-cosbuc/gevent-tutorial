9e3248640d730f40be4b421f83f8b7b42b56ab4d
Mircea Cosbuc <cosbuc@gmail.com>
2017-04-24 22:16:02 +0300

    Add queue examples

---
+++ queue_basic.py
@@ -0,0 +1,27 @@
+import gevent
+from gevent.queue import Queue
+
+tasks = Queue()
+
+
+def worker(n):
+    while not tasks.empty():
+        task = tasks.get()
+        print("Worker {} got task {}".format(n, task))
+        gevent.sleep(0)
+
+    print("Quitting time!")
+
+
+def boss():
+    for i in range(1, 25):
+        tasks.put_nowait(i)
+
+
+gevent.spawn(boss).join()
+
+gevent.joinall([
+    gevent.spawn(worker, 'steve'),
+    gevent.spawn(worker, 'john'),
+    gevent.spawn(worker, 'nancy')
+])
